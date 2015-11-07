14af1dbd25d0b62f639cdc0b8c902f135847898a
honzo0481 <gonzalesre@gmail.com>
2015-11-06 20:05:57 -0800

    update docker stack, fixed already provisioned msg

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,29 @@ apt-get update
 apt-get install -y git
 apt-get install -y tree
 apt-get install -y curl
+apt-get install -y wget
+apt-get install -y unzip
+
 if [ $(which docker) ]
   then
-    echo "no docker executable detected"
+    echo "docker executable detected...skipping install."
   else
-    curl -sSL https://get.docker.com/ | sh
+    wget -qO- https://get.docker.com/ | sh
     usermod -aG docker vagrant
 fi
 if [ $(which docker-compose) ]
   then
-    echo "no docker-compose executable detected"
+    echo "docker-compose executable detected...skipping install."
   else
-    curl -L https://github.com/docker/compose/releases/download/1.4.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.5.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 if [ $(which docker-machine) ]
   then
-    echo "no docker-machine executable detected"
+    echo "docker-machine executable detected...skipping install."
   else
-    curl -L https://github.com/docker/machine/releases/download/v0.4.0/docker-machine_linux-amd64 > /usr/local/bin/docker-machine
-    chmod +x /usr/local/bin/docker-machine
+    curl -L https://github.com/docker/machine/releases/download/v0.5.0/docker-machine_linux-amd64.zip >machine.zip && \
+    unzip machine.zip && \
+    rm machine.zip && \
+    mv -f docker-machine* /usr/local/bin
 fi
