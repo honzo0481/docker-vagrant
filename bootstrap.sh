ea4cc2d6acd4dc3a594d5ec89c83617e12243ecc
honzo0481 <gonzalesre@gmail.com>
2016-04-26 18:46:24 -0700

    upgraded to docker-compose 1.7

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,7 +28,7 @@ if [ $(which docker-compose) ]
   then
     echo "docker-compose executable detected...skipping install."
   else
-    curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
