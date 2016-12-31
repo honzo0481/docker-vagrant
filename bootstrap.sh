819525ae8341a2611bbe30105a411af8f1651262
honzo0481 <gonzalesre@gmail.com>
2016-12-30 22:57:16 -0800

    updated for use with xenial and the latest docker

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,36 +1,35 @@
 #!/usr/bin/env bash
 
+echo "bootstrapping..."
+
 # update apt sources
 apt-get update
-apt-get install -y apt-transport-https ca-certificates
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F76221572C52609D
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-touch /etc/apt/sources.list.d/docker.list
-chown vagrant:vagrant /etc/apt/sources.list.d/docker.list
-echo deb https://apt.dockerproject.org/repo ubuntu-trusty main> /etc/apt/sources.list.d/docker.list
-
-# update package mgr and install prereqs
+apt-get install apt-transport-https ca-certificates
+apt-key adv \
+               --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+               --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get purge lxc-docker
-apt-get install -y linux-image-extra-$(uname -r) apparmor
+
+# install xenial prerequisites
+apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 # install docker
 apt-get install -y docker-engine
 
-# install some stuff that i like
+# install some stuff i like
 apt-get install -y git curl wget unzip tree
-apt-get install -y git tree curl wget unzip
 
-# Create a Docker group
+#create a docker group
+groupadd docker
 usermod -aG docker vagrant
 
-# install docker-compose
+# install docker compose
 if [ $(which docker-compose) ]
   then
     echo "docker-compose executable detected...skipping install."
   else
-    curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/1.7.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 fi
 
@@ -39,7 +38,6 @@ if [ $(which docker-machine) ]
   then
     echo "docker-machine executable detected...skipping install."
   else
-    curl -L https://github.com/docker/machine/releases/download/v0.7.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
-    curl -L https://github.com/docker/machine/releases/download/v0.6.0/docker-machine-`uname -s`-`uname -m` > /usr/local/bin/docker-machine && \
+    curl -L https://github.com/docker/machine/releases/download/v0.8.2/docker-machine-`uname -s`-`uname -m` >/usr/local/bin/docker-machine && \
     chmod +x /usr/local/bin/docker-machine
 fi
